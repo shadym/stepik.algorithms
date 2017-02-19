042049ba101bf6e26bb50f421b6279a0adc36be2
Pavel Kartsel <pkartsel@gmail.com>
2017-02-20 01:12:48 +0300

    Count Equal elements optionally

--- src/Main/Search.cs
+++ src/Main/Search.cs
@@ -31,7 +31,7 @@ namespace Stepic.Algorithms
             return -1;
         }
 
-        public static int LessOrEqualCount<T>(T[] a, T key) where T:IComparable<T>
+        public static int LessCount<T>(T[] a, T key, bool countEqual = false) where T:IComparable<T>
         {
             var l = 0;
             var r = a.Length - 1;
@@ -41,7 +41,7 @@ namespace Stepic.Algorithms
                 var m = l + (r - l)/2;
                 if (a[m].CompareTo(key) == 0)
                 {
-                    return GetLastEqualElement(a, key, m, r);
+                    return countEqual ? GetLastEqualElement(a, key, m, r) : GetFirstEqualElement(a, key, m, l);
                 }
                 else if (a[m].CompareTo(key) < 0)
                 {
@@ -53,7 +53,7 @@ namespace Stepic.Algorithms
                         }
                         else if (a[m + 1].CompareTo(key) == 0)
                         {
-                            return GetLastEqualElement(a, key, m, r);
+                            return countEqual ? GetLastEqualElement(a, key, m + 1, r) : m + 1;
                         }
                         else if (a[m + 1].CompareTo(key) > 0)
                         {
@@ -62,16 +62,23 @@ namespace Stepic.Algorithms
                     }
                     else
                     {
-                        return -1;
+                        // all elements are less then key
+                        return r + 1;
                     }
                 }
                 else
                 {
+                    // a[m] > key
+
                     if (m - 1 >= l)
                     {
-                        if (a[m - 1].CompareTo(key) <= 0)
+                        if (a[m - 1].CompareTo(key) < 0)
                         {
                             return m;
+                        }
+                        else if (a[m - 1].CompareTo(key) == 0)
+                        {
+                            return countEqual ? m : GetFirstEqualElement(a, key, m - 1, l);
                         }
                         else
                         {
@@ -80,12 +87,12 @@ namespace Stepic.Algorithms
                     }
                     else
                     {
-                        return -1;
+                        return 0;
                     }                    
                 }
             }
 
-            return -1;
+            return 0;
         }
 
         private static int GetLastEqualElement<T>(T[] a, T key, int position, int lastPosition) where T: IComparable<T>
@@ -96,6 +103,16 @@ namespace Stepic.Algorithms
                 j++;
             }
             return position + j;
+        }
+
+        private static int GetFirstEqualElement<T>(T[] a, T key, int position, int firstPosition) where T: IComparable<T>
+        {
+            var j = 1;
+            while (position - j >= firstPosition && a[position - j].CompareTo(key) == 0)
+            {
+                j++;
+            }
+            return position - j + 1;
         }
     }
 }
