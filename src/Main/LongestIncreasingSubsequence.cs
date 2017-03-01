946b06102c4e2b5dd530558d5f4bd1c91fe60b13
Pavel Kartsel <pkartsel@gmail.com>
2017-03-02 00:27:48 +0300

    Implement Largest Not Increasing Subsequence

--- src/Main/LongestIncreasingSubsequence.cs
+++ src/Main/LongestIncreasingSubsequence.cs
@@ -100,6 +100,88 @@ namespace Stepic.Algorithms
 
             return len;
         }
+
+
+        public static int GetLongestNotIncreasingSubsequenceNLogN(int[] a)
+        {
+            if (a.Length < 2)
+            {
+                return a.Length;
+            }
+
+            var t = new int[a.Length];
+            var restore = new int[a.Length];
+            t[0] = 0;
+            restore[0] = -1;
+            var len = 1;
+
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (a[i] <= a[t[len - 1]])
+                {
+                    t[len] = i;
+                    restore[i] = t[len - 1];
+                    len++;
+                }
+                else if (a[i] > a[t[0]])
+                {
+                    t[0] = i;
+                    restore[i] = -1;
+                }
+                else if (a[i] != a[t[0]])
+                {
+                    // use binary search
+                    var pos = -1;
+                    var l = 0;
+                    var r = len;
+                    while (l < r)
+                    {
+                        var m = l + (r - l) / 2;
+
+                        if (a[t[m]] >= a[i])
+                        {
+                            if ((m < r + 1) && a[t[m + 1]] < a[i])
+                            {
+                                pos = m + 1;
+                                break;
+                            }
+                            else
+                            {
+                                l = m + 1;
+                            }
+                        }
+                        else
+                        {
+                            r = m;
+                        }
+                    }
+
+                    if (pos != -1)
+                    {
+                        restore[i] = t[pos - 1];
+                        t[pos] = i;
+                    }
+                }
+            }
+
+            Console.WriteLine(len);
+            PrintRestoreSubsequence(restore, len, t[len - 1]);
+
+            return len;
+        }
+
+        private static void PrintRestoreSubsequence(int[] restore, int length, int lastIndex)
+        {
+            var result = new int[length];
+
+            for (var i = length - 1; i >= 0; i--)
+            {
+                result[i] = lastIndex;
+                lastIndex = restore[lastIndex];
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
     }
 
     public static class Task825
